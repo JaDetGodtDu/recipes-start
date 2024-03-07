6a8130cef90a7173a06ae413d1ce79d5253b7cf6
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2024-03-07 15:32:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "https://jadetgodtdu.github.io/recipes-start",
-  base:"http://localhost:4173/recipes-start"
+  base: "https://jadetgodtdu.github.io/recipes-start",
+  //base:"http://localhost:4173/recipes-start"
 });
